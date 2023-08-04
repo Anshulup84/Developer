00436829ded6ee3f98a0c7b153db806579c1ee70
Anshulup84 <avenger720720@gmail.com>
2023-08-04 17:19:50 +0530

    working of javascript

--- greeting.js
+++
@@ -1,8 +0,0 @@
-function greet(name) {
-    return new Promise((resolve) => {
-        const greeting = `Hello, ${name}!`;
-        resolve(greeting);
-    });
-}
-
-greet("Anshul").then((message) => console.log(message));
